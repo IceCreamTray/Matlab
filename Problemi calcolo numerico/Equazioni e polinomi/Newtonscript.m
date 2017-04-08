cc57376035f12ef0074cd42a112a79f2036fdb49
God <strawberryentrypoint@gmail.com>
2017-04-08 18:22:49 +0200

    Formatting.

---
+++ Problemi calcolo numerico/Equazioni e polinomi/Newtonscript.m
@@ -0,0 +1,26 @@
+exprf = input('Inserire funzione tra apici');
+exprf1 = input('Inserire la derivatatra apici');
+x0 = input('Inserire il valore iniziale');
+toll = input ('Inserire la tolleranza');
+nmax = input('Inserire il numero massimo di iterate');
+
+f = inline(exprf);
+f1 = inline(exprf1);
+
+[xv, fxv, n, flag] = newtonfun(f, f1, x0, toll, nmax);
+
+if (n > nmax)
+	disp('Numero massimo iterate raggiunto')
+elseif (flag == 0)
+	disp('Ultima iterata approssimata');
+	disp(xv(n));
+	disp('Residuo ultima iterata');
+	disp(fxv(n));
+	disp('Indice iterata');
+	disp(n - 1);
+else
+	disp('La derivata si annulla!');
+end
+
+eass = abs(fxv - fxv(n));
+semilogy(eass, abs(fxv));
