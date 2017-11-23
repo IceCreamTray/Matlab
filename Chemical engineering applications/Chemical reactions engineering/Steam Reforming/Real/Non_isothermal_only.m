75e07ffb0f9b2cbf2c975a25a2059c919558164a
ice cube tray <icecubetray@users.noreply.github.com>
2017-11-23 17:27:32 +0100

    Moved boyfriend contribution.

--- Chemical engineering applications/Chemical reactions engineering/Steam Reforming/Real/Non_isothermal_only.m
+++ Chemical engineering applications/Chemical reactions engineering/Steam Reforming/Real/Non_isothermal_only.m
@@ -211,14 +211,4 @@ function Non_isothermal_reactor
 	ylabel('Enthalpy [KJ/mol]');
 	xlim([500 800]);
 	
-	%%
-	% Boyfriend contribution
-	lotsa = -10 : 0.01 : 10;
-	lo = 16 * (power(sin(lotsa), 3));
-	ve = (13 * cos(lotsa)) - (5 * cos(2 * lotsa)) - (2 * cos(3 * lotsa)) - (cos(4 * lotsa));
-	
-	figure
-	plot(lo,ve);
-	title('929');
-	
 end
