44a135931203368424bdfa8d7502db205e35184d
IceCreamTray <IceCreamTray@users.noreply.github.com>
2018-01-13 20:29:37 +0100

    Boyfriends so helpful

--- Chemical engineering applications/Chemical reactions engineering/Monolite/Batch_reactor.m
+++ Chemical engineering applications/Chemical reactions engineering/Monolite/Batch_reactor.m
@@ -4,7 +4,7 @@ Trials = xlsread('Expdata.xlsx');
 Trials_trimmed = Trials(8:end, :);
 
 %constants
-Rg = 8.314 %J/kmol
+Rg = 8.314 %J/K mol
 nu = -1;
 % Reaction is R-plus + Naoh = R-na + oh-
 % Data
@@ -23,7 +23,7 @@ porosity = 0.225;	%[-]
 set(0,'DefaultAxesColorOrder',[1 0 0; .1 1 .1; 0 0 1]);
 opt=optimset('Display','Iter');
 
-par0 = [0.1 0.1];
+par0 = [60 19500];
 
 
 global results_C1;
@@ -33,20 +33,33 @@ Tvec = [ 5 17.5 30.6 43] + 273.15;
 Tvec_len = length(Tvec);
 results_C1 = {};
 results_t1 = {};
+A_vec = zeros(1,Tvec_len);
+Ea_vec = zeros(1,Tvec_len);
+
 for Tidx = 1 : Tvec_len
 	temperature = Tvec(Tidx);
 	time = Trials_trimmed(:,1);	%s
-	coutsoda = Trials_trimmed(:, (Tidx + 1)) / 1e3;
+	coutsoda = Trials_trimmed(:, (Tidx + 1)) %mol/m^3;
+	for i = 1 : length(coutsoda)
+		if coutsoda(i) == 0
+			time = time(1:(i - 1));
+			coutsoda = coutsoda(1:(i - 1));
+			break;
+		end
+	end
 	cinsoda = coutsoda(1, 1);
 	
 	[par,fval] = fminsearch(@err, par0, opt, time, cinsoda, temperature, coutsoda, Tidx);
-	disp(fval);
-	disp(par);
+	A_vec(Tidx) = par(1);
+	Ea_vec(Tidx) = par(2);
 	plot(results_t1{Tidx},results_C1{Tidx},time,coutsoda,'s'),hold on
 	drawnow
 end
 
-disp('poop');
+pre_exp = mean(A_vec)
+act_energy = mean(Ea_vec)
+
+disp('poop love');
 	
 
 function S = err(par, time, cinsoda, temperature,coutsoda, index)
@@ -63,7 +76,7 @@ end
 
 function sol = Batch(par, time, cinsoda, temperature)
 	nu = -1;
-	Rg = 8.314; %J/kmol
+	Rg = 8.314; %J/ K mol
 	sol = ode15s(@BMi,time,cinsoda,[],par,temperature,Rg,nu);
 end
 	
