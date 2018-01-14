8e647313610e67ba13e46db17fa2fe9aa1581399
IceCreamTray <IceCreamTray@users.noreply.github.com>
2018-01-14 09:51:14 +0100

    Fixed total mtc

--- Chemical engineering applications/Chemical reactions engineering/Monolite/TotalMTC.m
+++ Chemical engineering applications/Chemical reactions engineering/Monolite/TotalMTC.m
@@ -36,49 +36,13 @@ Tvec = [ 5 17.5 30.6 43] + 273.15;
 Tvec_len = length(Tvec);
 results_C1 = {};
 results_t1 = {};
-A_vec = zeros(1,Tvec_len);
-Ea_vec = zeros(1,Tvec_len);
-power_vec = zeros(1,Tvec_len);
-for Tidx = 1 : Tvec_len
-	temperature = Tvec(Tidx);
-	time = Trials_trimmed(:,1);	%s
-	coutsoda = Trials_trimmed(:, (Tidx + 1))/10^6 %mol/cm^3;
-	for i = 1 : length(coutsoda)
-		if coutsoda(i) == 0
-			time = time(1:(i - 1));
-			coutsoda = coutsoda(1:(i - 1));
-			break;
-		end
-	end
-	cinsoda = coutsoda(1, 1);
-	
-	[par,fval] = fminsearch(@err, par0, opt, time, cinsoda, temperature, coutsoda, Tidx);
-	disp(par)
-	%power_vec(Tidx) = par(3);
-	plot(results_t1{Tidx},results_C1{Tidx},time,coutsoda,'s'),hold on
-	drawnow
-end
-
-
-
-disp('poop love');
-	
-
-function S = err(par, time, cinsoda, temperature,coutsoda, index)
-	sol = Batch(par, time, cinsoda, temperature);
-	Ccalc = deval(sol, time)';
-	S = norm(Ccalc - coutsoda);
-	t1   = linspace(min(time),max(time),100); 
-	C1   = deval(sol,t1);
-	global results_C1;
-	global results_t1;
-	results_t1{index} = t1;
-	results_C1{index} = C1;
-end
+% A_vec = zeros(1,Tvec_len);
+% Ea_vec = zeros(1,Tvec_len);
+% power_vec = zeros(1,Tvec_len);
 % for Tidx = 1 : Tvec_len
-% temperature = Tvec(Tidx);
-% time = Trials_trimmed(:,1);	%s
-% coutsoda = Trials_trimmed(:, (Tidx + 1))/10^6; %mol/cm^3;
+% 	temperature = Tvec(Tidx);
+% 	time = Trials_trimmed(:,1);	%s
+% 	coutsoda = Trials_trimmed(:, (Tidx + 1))/10^6 %mol/cm^3;
 % 	for i = 1 : length(coutsoda)
 % 		if coutsoda(i) == 0
 % 			time = time(1:(i - 1));
@@ -86,24 +50,60 @@ end
 % 			break;
 % 		end
 % 	end
-% cinsoda = coutsoda(1, 1);
-% solution = Batch(time,cinsoda,temperature);
-% plot(solution.x,solution.y),hold on
-% scatter(time,coutsoda)
+% 	cinsoda = coutsoda(1, 1);
+% 	
+% 	[par,fval] = fminsearch(@err, par0, opt, time, cinsoda, temperature, coutsoda, Tidx);
+% 	disp(par)
+% 	%power_vec(Tidx) = par(3);
+% 	plot(results_t1{Tidx},results_C1{Tidx},time,coutsoda,'s'),hold on
+% 	drawnow
 % end
-function sol = Batch(par,time, cinsoda, temperature)
+
+
+
+disp('poop love');
+	
+
+% function S = err(par, time, cinsoda, temperature,coutsoda, index)
+% 	sol = Batch(par, time, cinsoda, temperature);
+% 	Ccalc = deval(sol, time)';
+% 	S = norm(Ccalc - coutsoda);
+% 	t1   = linspace(min(time),max(time),100); 
+% 	C1   = deval(sol,t1);
+% 	global results_C1;
+% 	global results_t1;
+% 	results_t1{index} = t1;
+% 	results_C1{index} = C1;
+% end
+for Tidx = 1 : Tvec_len
+temperature = Tvec(Tidx);
+time = Trials_trimmed(:,1);	%s
+coutsoda = Trials_trimmed(:, (Tidx + 1))/10^6; %mol/cm^3;
+	for i = 1 : length(coutsoda)
+		if coutsoda(i) == 0
+			time = time(1:(i - 1));
+			coutsoda = coutsoda(1:(i - 1));
+			break;
+		end
+	end
+cinsoda = coutsoda(1, 1);
+solution = Batch(time,cinsoda,temperature);
+plot(solution.x,solution.y),hold on
+scatter(time,coutsoda)
+end
+function sol = Batch(time, cinsoda, temperature)
 	nu = -1;
 	Rg = 8.314; %J/ K mol
 	porosity = 0.225;	%[-]
 	soda = 470*10^3	%cm^3
 	resin = 10*10^3	%cm^3
 	vol = soda+resin;	%cm^3
-	xsoda = soda/vol;
-	xresin = resin/vol;
+	fiL = soda/vol;
+	fiS = resin/vol;
 	porosity = 0.225;	%[-]
 	D = 650e-4;             % Diametro delle resine - [cm]
 	aS = 6/D;               % Area specifica del solido - [1/cm]
-	aL = aS*(1-porosity)/porosity;
+	aL = fiS*aS/fiL;        % Area specifica del liquido - [1/m]
 	mu = 0.087;	%cpoise
 	diff = Rg*10^-3*temperature/(96500*(1/50.1+1/197.6));
 	rho = 2.13*10^-3; %Kg/cm^3
@@ -112,12 +112,12 @@ function sol = Batch(par,time, cinsoda, temperature)
 	Sc = mu/rho/diff;
 	Sh = 2 + 0.44*Rep^0.5*Sc^0.38;
 	hm = Sh * diff/D; %cm/s
-	sol = ode15s(@BMi,time,cinsoda,[],par,temperature,Rg,nu,aL,hm);
+	sol = ode15s(@BMi,time,cinsoda,[],temperature,Rg,nu,aL,hm);
 end
 	
-function Cprimo = BMi(time,c,par,Tin,Rg,nu,aL,hm)
+function Cprimo = BMi(time,c,Tin,Rg,nu,aL,hm)
 	%k = A*exp(-Ea/Rg/Tin);
-	R = hm*c;
+	R = hm*c*aL;
 	r = nu*R;
 	Cprimo = r';
 end
